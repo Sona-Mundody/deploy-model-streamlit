7fd578d95af4a4adecd01ac04adfaeb56a99f869
Sona-Mundody <77203908+Sona-Mundody@users.noreply.github.com>
2024-10-06 21:37:34 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -22,8 +22,8 @@ SOURCES_LIST = [IMAGE, VIDEO, WEBCAM, RTSP, YOUTUBE]
 
 # Images config
 IMAGES_DIR = ROOT / 'images'
-DEFAULT_IMAGE = IMAGES_DIR / 'office_4.jpg'
-DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
+DEFAULT_IMAGE = IMAGES_DIR / 'uploaded.jpg'
+DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'detected.jpg'
 
 # Videos config
 VIDEO_DIR = ROOT / 'videos'
